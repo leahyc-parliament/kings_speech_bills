ba049b2ecf60fdf1fadb9bc24d46309a27b67f2a
Catherine Leahy <leahyc@parliament.uk>
2026-05-29 17:47:24 +0100

    timestamp fixed in file name

--- gov_bill_alerts.R
+++ gov_bill_alerts.R
@@ -2,7 +2,7 @@ install.packages("tidyverse")
 library(tidyverse)
 
 original_gov_bills <- read.csv("back_up_data/gov_bills_29_05_2026.csv")
-  # slice(-c(1, 2, 3, 4)) #only for testing, remove eventually
+# slice(-c(1, 2, 3, 4)) #only for testing, remove eventually
 
 new_gov_bills <- read.csv("https://api.parliament.uk/bill-papers/bills.csv") |>
   mutate(
@@ -28,14 +28,17 @@ new_gov_bills <- read.csv("https://api.parliament.uk/bill-papers/bills.csv") |>
       )),
     (`Originating.session` %in% "59/2")
   ) |>
-  select(`bill_title` = `Bill.short.title`, `bill_id`) |> 
+  select(`bill_title` = `Bill.short.title`, `bill_id`) |>
   anti_join(original_gov_bills, by = "bill_id")
 
-write.csv(new_gov_bills, "new_gov_bills_", Sys.Date(), ".csv")
+write.csv(
+  new_gov_bills,
+  paste0("new_gov_bills_", Sys.Date(), ".csv"),
+  row.names = FALSE
+)
 
 # write.csv(
 #   new_gov_bills,
 #   paste0("C:\\Users\\leahyc\\OneDrive - UK Parliament\\Government Bills Alerts\\new_gov_bills_", Sys.Date(), ".csv"),
 #   row.names = FALSE
 # )
-
