7ab26ac62b5f3f602c0bdced43f0680dbd80c582
Catherine Leahy <leahyc@parliament.uk>
2026-05-29 17:15:12 +0100

    save location fixed

--- gov_bill_alerts.R
+++ gov_bill_alerts.R
@@ -33,10 +33,11 @@ new_gov_bills <- read.csv("https://api.parliament.uk/bill-papers/bills.csv") |>
   select(`bill_title` = `Bill.short.title`, `bill_id`) |> 
   anti_join(original_gov_bills, by = "bill_id")
 
+write.csv(new_gov_bills, "new_gov_bills_", Sys.Date(), ".csv")
 
-write.csv(
-  new_gov_bills,
-  paste0("C:\\Users\\leahyc\\OneDrive - UK Parliament\\Government Bills Alerts\\new_gov_bills_", Sys.Date(), ".csv"),
-  row.names = FALSE
-)
+# write.csv(
+#   new_gov_bills,
+#   paste0("C:\\Users\\leahyc\\OneDrive - UK Parliament\\Government Bills Alerts\\new_gov_bills_", Sys.Date(), ".csv"),
+#   row.names = FALSE
+# )
 
