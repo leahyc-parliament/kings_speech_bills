e8cc63e0e1f27bac2eeead8d4e7a8bb50ec28f1f
Catherine Leahy <leahyc@parliament.uk>
2026-06-15 14:33:07 +0100

    gov bill alerts changed to keep track of bills already accounted for in app

--- gov_bill_alerts.R
+++ gov_bill_alerts.R
@@ -1,8 +1,8 @@
 install.packages("tidyverse")
 library(tidyverse)
 
-original_gov_bills <- read.csv("back_up_data/gov_bills_29_05_2026.csv") |> 
-slice(-c(1, 2, 3, 4)) #only for testing, remove eventually
+original_gov_bills <- read.csv("back_up_data/gov_bills_29_05_2026.csv")  
+# slice(-c(1, 2, 3, 4)) #only for testing, remove eventually
 
 new_gov_bills <- read.csv("https://api.parliament.uk/bill-papers/bills.csv") |>
   mutate(
@@ -28,19 +28,11 @@ new_gov_bills <- read.csv("https://api.parliament.uk/bill-papers/bills.csv") |>
       )),
     (`Originating.session` %in% "59/2")
   ) |>
-  select(`bill_title` = `Bill.short.title`, `bill_id`) |>
-  anti_join(original_gov_bills, by = "bill_id")
+  select(`bill_title` = `Bill.short.title`, `bill_id`) 
+  # anti_join(original_gov_bills, by = "bill_id")
 
 write.csv(
   new_gov_bills,
-  "new_gov_bills.csv",
+  "accounted_for_gov_bills.csv",
   row.names = FALSE
 )
-
-# write.csv(
-#   new_gov_bills,
-#   paste0("C:\\Users\\leahyc\\OneDrive - UK Parliament\\Government Bills Alerts\\first_new_gov_bills_", Sys.Date(), ".csv"),
-#   row.names = FALSE
-# )
-
-  # paste0("new_gov_bills_", Sys.Date(), ".csv"),
